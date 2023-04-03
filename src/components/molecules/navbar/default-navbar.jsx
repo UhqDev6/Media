011b2b2256ab7fc8870f145a78a59dadc9a14089
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-04-04 03:56:14 +0800

    added news details to the press release item category & fix details blog item category

--- src/components/molecules/navbar/default-navbar.jsx
+++ src/components/molecules/navbar/default-navbar.jsx
@@ -9,7 +9,7 @@ export default function DefaultNavbar() {
       <ul className="flex justify-between gap-8">
         {
           navLink.map(({ link, name }) => (
-            <Link key={`${name}-defaultNavbar`} href={link}>
+            <Link key={`${name}-defaultNavbar`} href={link} prefetch={false}>
               <li
                 className={`${router.pathname === link ? ('bg-[#B1B2FF] p-2 rounded-full text-white')
                   : ('p-2 text-gray-500 capitalize inline-block relative cursor-pointer transition-all duration-500 before:content-[ ] before:absolute before:bottom-2 before:left-0 before:w-0 before:h-1 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-[#B1B2FF] before:bg-fuchsia-300 before:to-bg-fuchsia-300 hover:before:w-full hover:before:opacity-100')
